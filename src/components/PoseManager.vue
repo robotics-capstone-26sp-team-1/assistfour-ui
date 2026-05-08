d39348b1cd55bc3437f519639bf2385ad18c96a1
Kenneth Yang <kjy5@uw.edu>
2026-05-07 23:55:44 -0700

    Reject failed poses

--- src/components/PoseManager.vue
+++ src/components/PoseManager.vue
@@ -33,8 +33,12 @@ function addPose() {
   getPoseService.callService(
     request,
     (response) => {
-      emit('onPoseSave', poseName.value, response)
-      poseName.value = ''
+      if (response.success) {
+        emit('onPoseSave', poseName.value, response)
+        poseName.value = ''
+      } else {
+        console.error(response.message)
+      }
     },
     (error) => {
       console.error(`Failed to call /get_pose: ${error}`)
@@ -46,7 +50,7 @@ function deletePose() {
   selectedPose.value = undefined
 }
 function sendPose() {
-  emit('onPoseSent', { name: selectedPose.value!, link: selectedFrame.value})
+  emit('onPoseSent', { name: selectedPose.value!, link: selectedFrame.value })
 }
 </script>
 
